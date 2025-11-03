f1a3d4e7f13983b294545fec71583bf55e66372a
baiba <baiba@DESKTOP-FA0TIP5>
2025-11-03 22:58:35 +0200

    Add project files.

---
+++ Data/Models/Pomodor.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pilens.Data.Models
+{
+    public class Pomodoro
+    {
+        [Key]
+        public int Id { get; set; }
+        public int DurationInMinutes { get; set; }
+
+        public Pomodoro() { }
+
+        public Pomodoro(int id, int durationInMinutes)
+        {
+            Id = id;
+            DurationInMinutes = durationInMinutes;
+        }
+    }
+}
