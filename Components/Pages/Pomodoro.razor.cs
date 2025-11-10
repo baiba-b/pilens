e9c1a49c9212d994c71b168ad3147da44a98ae81
baiba <baiba@DESKTOP-FA0TIP5>
2025-11-10 23:36:01 +0200

    Papildus Pomodoro funkcijas: pauze/pārtraukt darbību

--- Components/Pages/Pomodoro.razor.cs
+++ Components/Pages/Pomodoro.razor.cs
@@ -6,15 +6,21 @@ namespace Pilens.Components.Pages
     public partial class Pomodoro
     {
         private static System.Timers.Timer aTimer;
+        private static System.Timers.Timer pTimer;
         private int InputMinutes { get; set; } = 25;
         private int InputPauseMinutes { get; set; } = 5;
         private int InputLongPauseMinutes { get; set; } = 20; //vajadzēs iespēju skippot pauzi + 4 sesijai noņemt īso pauzi
         private int InputSessionAmount { get; set; } = 4; //  (viena sesija = 1 pomodoro + pauze)
+
+        private int InputSessionLongPause { get; set; } = 4;
         private int RemainingSeconds { get; set; } = 0;
         private string DisplayStatus { get; set; } = "Stop";
         private string ErrorMessage { get; set; }
         private bool StartBtnPressed { get; set; } = false;
         private bool StopBtnPressed  { get; set; } = false;
+
+        private bool IsPause { get; set; } = false;
+        private int CurrSession { get; set; } = 0;
         private string DisplayTime =>
             TimeSpan.FromSeconds(RemainingSeconds).ToString(@"mm\:ss");
 
@@ -36,22 +42,37 @@ namespace Pilens.Components.Pages
             aTimer.Enabled = true;
             StartBtnPressed = true;
         }
+        private void SetPause(int min) //koda implementācijas piemērs ņemts no https://learn.microsoft.com/en-us/dotnet/api/system.timers.timer?view=net-9.0  un https://learn.microsoft.com/en-us/aspnet/core/blazor/components/synchronization-context?view=aspnetcore-9.0 
+        {
+            RemainingSeconds = min * 60;
+
+            pTimer = new System.Timers.Timer(1000);
+            pTimer.Elapsed += UpdatePauseTimer;
+            pTimer.AutoReset = true;
+            pTimer.Enabled = true;
+            StartBtnPressed = true;
+        }
+        private void UpdatePauseTimer(Object source, ElapsedEventArgs e)
+        {
+            if (RemainingSeconds > 0)
+            {
+                RemainingSeconds--;
+                InvokeAsync(StateHasChanged);
+            }
+            else
+            {
+                IsPause = false;
+                SetTimer();
+            }
+        }
 
         private void StopTimer(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
         {
-            //if(DisplayStatus == "Stop")
-            //{
-                aTimer.Stop();
-                DisplayStatus = "Continue";
-                StopBtnPressed = true;
-            //}
-            //else
-            //{
-            //    aTimer.Start();
-            //    DisplayStatus = "Stop";
-            //}
-            //StateHasChanged();
-
+                
+           if(!IsPause) aTimer.Stop();
+           else pTimer.Stop();
+            DisplayStatus = "Continue";
+            StopBtnPressed = true;
         }
         private void UpdateTimer(Object source, ElapsedEventArgs e)
         {
@@ -62,25 +83,42 @@ namespace Pilens.Components.Pages
             }
             else
             {
+                CurrSession++;
+                IsPause = true;
+                if (CurrSession < InputSessionAmount && CurrSession < 4) SetPause(InputPauseMinutes);
+                else SetPause(InputLongPauseMinutes);
                 aTimer.Stop();
                 aTimer.Dispose();
             }
         }
         private void ResetTimer(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
         {
-            aTimer.Stop();
-            aTimer.Dispose();
-            aTimer = null;
+            if (!IsPause)
+            {
+                aTimer.Stop();
+                aTimer.Dispose();
+                aTimer = null;
+            }
+            else
+            {
+                pTimer.Stop();
+                pTimer.Dispose();
+                pTimer = null;
+            }
+            IsPause = false;
+             
+
             InputMinutes = 25;
             RemainingSeconds = 0;
             StartBtnPressed = false;
             StopBtnPressed = false;
-            DisplayStatus = "Stop";
+            CurrSession = 0;
             InvokeAsync(StateHasChanged);
         }
         private void ContinueTimer(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
         {
-            aTimer.Start();
+            if (!IsPause) aTimer.Start();
+            else pTimer.Start();
             StopBtnPressed = false;
             InvokeAsync(StateHasChanged);
         }
