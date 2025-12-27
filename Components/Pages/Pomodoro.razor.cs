bae9737b85388207277598df67c77a7e0fba8b55
baiba <baiba@DESKTOP-FA0TIP5>
2025-12-27 20:46:30 +0200

    Pomodoro kļūdu izskate

--- Components/Pages/Pomodoro.razor.cs
+++ Components/Pages/Pomodoro.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MudBlazor;
 using Pilens.Data;
 using Pilens.Data.DTO;
@@ -16,13 +17,17 @@ namespace Pilens.Components.Pages
         private static System.Timers.Timer? pTimer;
         private int RemainingSeconds { get; set; } = 0;
         private string DisplayStatus { get; set; } = "Stop";
-        private string? ErrorMessage { get; set; }
         private bool StartBtnPressed { get; set; } = false;
         private bool StopBtnPressed { get; set; } = false;
         private bool IsShortPause { get; set; } = false;
         private bool IsLongPause { get; set; } = false;
+        string ErrorMessage = string.Empty;
         private bool isDone { get; set; } = false;
+        bool success;
+        private MudForm? pomodoroForm;
+        
         private int CurrSession { get; set; } = 0;
+        string PomodoroReqError = "Šis ir obligāts atribūts!";
 
         PomodoroDTO pomodoroData = new();
         private readonly object _timerLock = new(); //lai taimeris netruprina atjaunoties kamēr iestata pauzi
@@ -35,27 +40,78 @@ namespace Pilens.Components.Pages
 
         public bool IsRunning => StartBtnPressed;
 
+       
+        private string PomodoroValidation(int arg)
+        {
+            string errorMessage = string.Empty;
+
+            if (arg < 1)
+            {
+                errorMessage = "Lūdzu ievadi pozitīvu skaitli.";
+                return errorMessage;
+            }
+            else return errorMessage;
+        }
+
         protected override async Task OnInitializedAsync()
         {
+            success = true;
             var userId = await getUserId();
-            if (string.IsNullOrWhiteSpace(userId))
+            if (userId == null)
             {
+                string errorMessage = "Neizdevās identificēt lietotāju.";
+                SnackbarService.Add(errorMessage, Severity.Error);
                 return;
             }
 
-            await using var db = await DbContextFactory.CreateDbContextAsync();
-            var existingPomodoro = await db.Pomodoros
-                .AsNoTracking()
-                .FirstOrDefaultAsync(p => p.UserID == userId);
-
-            if (existingPomodoro is not null)
+            try
             {
-                pomodoroData = new PomodoroDTO(existingPomodoro);
-                pomodoroData.UserID = userId;
-                RemainingSeconds = pomodoroData.Minutes * 60;
+                await using var db = await DbContextFactory.CreateDbContextAsync();
+                var existingPomodoro = await db.Pomodoros
+               .AsNoTracking()
+               .FirstOrDefaultAsync(p => p.UserID == userId);
+                if (existingPomodoro is not null)
+                {
+                    pomodoroData = new PomodoroDTO(existingPomodoro);
+                    pomodoroData.UserID = userId;
+                    RemainingSeconds = pomodoroData.Minutes * 60;
+                }
             }
-        }
+            catch (Exception)
+            {
+                string errorMessage = "Kļūda ielādējot datus.";
+                SnackbarService.Add(errorMessage, Severity.Error);
+                return;
+            }
 
+        }
+        /// <summary>
+        /// Funkcija, kas izveido un sāk taimeri
+        /// </summary>
+        private async Task SetTimer() //System.Timer funkciju implementācijas piemērs & apraksts ņemts no https://learn.microsoft.com/en-us/dotnet/api/system.timers.timer?view=net-9.0  un https://learn.microsoft.com/en-us/aspnet/core/blazor/components/synchronization-context?view=aspnetcore-9.0 
+        {
+            await pomodoroForm.Validate();
+            if (pomodoroForm.IsValid == false)
+            {
+                SnackbarService.Add("Lūdzu, labojiet veidlapas kļūdas pirms sākat Pomodoro taimeri.", Severity.Error);
+                return;
+            }
+            string errorMessage = string.Empty; ;
+            if (aTimer != null)
+            {
+                errorMessage = "Kļūda atrodot taimeri!";
+                SnackbarService.Add(errorMessage, Severity.Error);
+            }
+            RemainingSeconds = pomodoroData.Minutes * 60;
+
+            // Create a timer with a second interval.
+            aTimer = new System.Timers.Timer(1000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += UpdateTimer;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            StartBtnPressed = true;
+        }
         public void AddSessions(int sessions)
         {
             if (sessions <= 0)
@@ -73,40 +129,15 @@ namespace Pilens.Components.Pages
                 InitializeAndStartSessions(sessions);
             }
         }
-
-        // Funkcija, kas izveido  un sāk taimeri
-        private void SetTimer() //System.Timer funkciju implementācijas piemērs & apraksts ņemts no https://learn.microsoft.com/en-us/dotnet/api/system.timers.timer?view=net-9.0  un https://learn.microsoft.com/en-us/aspnet/core/blazor/components/synchronization-context?view=aspnetcore-9.0 
-        {
-            if (aTimer != null) return;
-            RemainingSeconds = pomodoroData.Minutes * 60;
-            ErrorMessage = string.Empty;
-            if (RemainingSeconds <= 0)
-            {
-                ErrorMessage = "Lūdzu ievadi pozitīvu minūšu skaitu.";
-                return;
-            }
-           
-            // Create a timer with a two second interval.
-            aTimer = new System.Timers.Timer(1000);
-            // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += UpdateTimer;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
-            StartBtnPressed = true;
-            
-        }
-
         // Funkcija, kas sāk pauzes taimeri
         private void SetPause(int min) //koda implementācijas piemērs ņemts no https://learn.microsoft.com/en-us/dotnet/api/system.timers.timer?view=net-9.0  un https://learn.microsoft.com/en-us/aspnet/core/blazor/components/synchronization-context?view=aspnetcore-9.0 
         {
             RemainingSeconds = min * 60;
-
             pTimer = new System.Timers.Timer(1000);
             pTimer.Elapsed += UpdatePauseTimer;
             pTimer.AutoReset = true;
             pTimer.Enabled = true;
         }
-
         // Funkcija, kas atjauno UI un atlikošu laiku atbilstoši darba taimerim
         private void UpdateTimer(object source, ElapsedEventArgs e)
         {
@@ -119,6 +150,12 @@ namespace Pilens.Components.Pages
                 }
                 else
                 {
+                    if (aTimer == null)
+                    {
+                        string errorMessage = "Kļūda atrodot taimeri!";
+                        SnackbarService.Add(errorMessage, Severity.Error);
+                        return;
+                    }
                     aTimer.Stop();
                     aTimer.Dispose();
                     aTimer = null;
@@ -135,9 +172,7 @@ namespace Pilens.Components.Pages
                         SetPause(pomodoroData.LongPauseMinutes);
                     }
                     else
-                    {                         //Restartē visu pēc visu sesiju pabeigšanas
-                        
-
+                    {   //Restartē visu pēc visu sesiju pabeigšanas
                         RemainingSeconds = 0;
                         StartBtnPressed = false;
                         StopBtnPressed = false;
@@ -160,6 +195,12 @@ namespace Pilens.Components.Pages
                 }
                 else
                 {
+                    if (pTimer == null)
+                    {
+                        string errorMessage = "Kļūda atrodot taimeri!";
+                        SnackbarService.Add(errorMessage, Severity.Error);
+                        return;
+                    }
                     pTimer?.Stop();
                     pTimer?.Dispose();
                     pTimer = null;
@@ -169,18 +210,29 @@ namespace Pilens.Components.Pages
                 }
             }
         }
-
         // Funkcija, kas atiestata taimeri uz sākotnējo stāvokli
         private void RestartTimer()
         {
             if (!IsShortPause && !IsLongPause)
             {
+                if (aTimer == null)
+                {
+                    ErrorMessage = "Kļūda atrodot taimeri!";
+                    SnackbarService.Add(ErrorMessage, Severity.Error);
+                    return;
+                }
                 aTimer.Stop();
                 aTimer.Dispose();
                 aTimer = null;
             }
             else
             {
+                if (pTimer == null)
+                {
+                    ErrorMessage = "Kļūda atrodot taimeri!";
+                    SnackbarService.Add(ErrorMessage, Severity.Error);
+                    return;
+                }
                 pTimer.Stop();
                 pTimer.Dispose();
                 pTimer = null;
@@ -194,16 +246,20 @@ namespace Pilens.Components.Pages
             CurrSession = 0;
             InvokeAsync(StateHasChanged);
         }
-
         private void SkipPause(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
         {
+            if (pTimer == null)
+            {
+                string errorMessage = "Kļūda atrodot taimeri!";
+                SnackbarService.Add(errorMessage, Severity.Error);
+                return;
+            }
             pTimer.Stop();
             pTimer.Dispose();
             pTimer = null;
             DeactivateActivePause();
             SetTimer();
         }
-
         private void DeactivateActivePause()
         {
             if (IsShortPause)
@@ -221,17 +277,53 @@ namespace Pilens.Components.Pages
         //Funkcija, kas aptur taimeri
         private void PauseTimer(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
         {
-                
-           if(!IsShortPause && !IsLongPause) aTimer.Stop();
-           else pTimer.Stop();
-           StopBtnPressed = true;
+
+            if (!IsShortPause && !IsLongPause)
+            {
+                if (aTimer == null)
+                {
+                    string errorMessage = "Kļūda atrodot taimeri!";
+                    SnackbarService.Add(errorMessage, Severity.Error);
+                    return;
+                }
+                aTimer.Stop();
+            }
+            else
+            {
+                if (pTimer == null)
+                {
+                    string errorMessage = "Kļūda atrodot taimeri!";
+                    SnackbarService.Add(errorMessage, Severity.Error);
+                    return;
+                }
+                pTimer.Stop();
+            }
+            StopBtnPressed = true;
         }
-      
+
         // Funkcija, kas turpina taimeri pēc apturēšanas
         private void ContinueTimer(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
         {
-            if (!IsShortPause && !IsLongPause) aTimer.Start();
-            else pTimer.Start();
+            if (!IsShortPause && !IsLongPause)
+            {
+                if (aTimer == null)
+                {
+                    string errorMessage = "Kļūda atrodot taimeri!";
+                    SnackbarService.Add(errorMessage, Severity.Error);
+                    return;
+                }
+                aTimer.Start();
+            }
+            else
+            {
+                if (pTimer == null)
+                {
+                    string errorMessage = "Kļūda atrodot taimeri!";
+                    SnackbarService.Add(errorMessage, Severity.Error);
+                    return;
+                }
+                pTimer.Start();
+            }
             StopBtnPressed = false;
             InvokeAsync(StateHasChanged);
         }
@@ -246,16 +338,23 @@ namespace Pilens.Components.Pages
                 if (RemainingSeconds > pomodoroData.AdjustedMin * 60) RemainingSeconds -= pomodoroData.AdjustedMin * 60;
                 else RemainingSeconds = 0;
             }
-                
-        
         }
         async Task<string> getUserId()
         {
-            var user = (await _authenticationStateProvider.GetAuthenticationStateAsync()).User;
-            var UserId = user.FindFirst(u => u.Type.Contains("nameidentifier"))?.Value;
-            return UserId;
+            try
+            {
+                var user = (await _authenticationStateProvider.GetAuthenticationStateAsync()).User;
+                var UserId = user.FindFirst(u => u.Type.Contains("nameidentifier"))?.Value;
+               
+                return UserId;
+            }
+            catch (Exception)
+            {
+                string errorMessage = "Neizdevās identificēt lietotāju.";
+                SnackbarService.Add(errorMessage, Severity.Error);
+                return string.Empty;
+            }
         }
-
 
         public void InitializeAndStartSessions(int sessions)
         {
@@ -265,15 +364,22 @@ namespace Pilens.Components.Pages
             StartBtnPressed = true;
             SetTimer();
         }
+        //TODO: Pārskatīt kļūdu apstrādi
         private async Task SavePomodoroData(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
         {
-            var userId = await getUserId();
-            if (string.IsNullOrWhiteSpace(userId))
+            await pomodoroForm.Validate();
+            if(pomodoroForm.IsValid == false)
             {
-                SnackbarService.Add("Neizdevās identificēt lietotāju.", Severity.Error);
+                SnackbarService.Add("Lūdzu, ievadiet korektus datus pirms saglabāšanas.", Severity.Error);
                 return;
             }
-
+            var userId = await getUserId();
+            if (userId == null)
+            {
+                string errorMessage = "Neizdevās identificēt lietotāju.";
+                SnackbarService.Add(errorMessage, Severity.Error);
+                return;
+            }
             pomodoroData.UserID = userId;
 
             try
